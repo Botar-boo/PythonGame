cab474299437e9b68b09c3104d848464a1894f26
Botar <ishanov.bd@gmail.com>
2021-04-19 20:43:27 +0300

    2nd_iteration

--- My_Game.py
+++ My_Game.py
@@ -7,11 +7,20 @@ FPS = 60
 currentFPS = 0
 score = 0
 whiteSpace = 17
+littleSpace = 4
+midSpace = 10
+bigSpace = 15
+buttonSpace = 14
 buttonWidth = 99
+buttonY = 400
+fontScoreSize = 36
+fontButtonSize = 15
+winScore = 100000
 worker = [0, 0, 0]
-
-# Задаем цвета
-global WHITE 
+workerPrice = [100, 200, 500]
+workerProd = [1, 3, 9]
+text1Coord = [90, 50]
+global WHITE
 WHITE = (255, 255, 255)
 
 # Создаем игру и окно
@@ -22,8 +31,9 @@ pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
 buttonImage = pygame.image.load('Button.png')
 background = pygame.image.load('Background.jpg')
-f1 = pygame.font.Font(None, 36)
-f2 = pygame.font.Font(None, 15)
+fontScore = pygame.font.Font(None, fontScoreSize)
+fontButton = pygame.font.Font(None, fontButtonSize)
+
 
 # Классы : Кнопки и Клик
 class Button(pygame.sprite.Sprite):
@@ -40,46 +50,49 @@ class Touch(pygame.sprite.Sprite):
         self.rect = pygame.Rect(x, y, 1, 1)
         self.rect.center = (x, y)
 
+
 def checkTouch(score, sprites, Click, worker):
-    if score >= 100 and pygame.sprite.collide_rect(sprites[0], Click):
+    if score >= workerPrice[0] and pygame.sprite.collide_rect(sprites[0], Click):
         worker[0] += 1
-        score -= 100
-    if score >= 200 and pygame.sprite.collide_rect(sprites[1], Click):
+        score -= workerPrice[0]
+    if score >= workerPrice[1] and pygame.sprite.collide_rect(sprites[1], Click):
         worker[1] += 1
-        score -= 200
-    if score >= 500 and pygame.sprite.collide_rect(sprites[2], Click):
+        score -= workerPrice[1]
+    if score >= workerPrice[2] and pygame.sprite.collide_rect(sprites[2], Click):
         worker[2] += 1
-        score -= 500
+        score -= workerPrice[2]
     return score
+
 
 def blitAll(background, text1, screen, buttonText, button1, button2, button3):
     screen.blit(background, (0, 0))
-    screen.blit(text1, (90, 50))
+    screen.blit(text1, (text1Coord[0], text1Coord[1]))
     all_sprites.draw(screen)
-    screen.blit(buttonText[0][0], (button1.rect.x + 4, button1.rect.y + 4))
-    screen.blit(buttonText[0][1], (button1.rect.x + 10,
-                button1.rect.y + 15 + 4))
-    screen.blit(buttonText[1][0], (button2.rect.x + 4, button2.rect.y + 4))
-    screen.blit(buttonText[1][1], (button2.rect.x + 4,
-                button2.rect.y + 15 + 4))
-    screen.blit(buttonText[2][0], (button3.rect.x + 4, button3.rect.y + 4))
-    screen.blit(buttonText[2][1], (button3.rect.x + 4,
-                button3.rect.y + 15 + 4))
+    screen.blit(buttonText[0][0], (button1.rect.x + littleSpace, button1.rect.y + littleSpace))
+    screen.blit(buttonText[0][1], (button1.rect.x + midSpace,
+                button1.rect.y + bigSpace + littleSpace))
+    screen.blit(buttonText[1][0], (button2.rect.x + littleSpace, button2.rect.y + littleSpace))
+    screen.blit(buttonText[1][1], (button2.rect.x + littleSpace,
+                button2.rect.y + bigSpace + littleSpace))
+    screen.blit(buttonText[2][0], (button3.rect.x + littleSpace, button3.rect.y + littleSpace))
+    screen.blit(buttonText[2][1], (button3.rect.x + littleSpace,
+                button3.rect.y + bigSpace + littleSpace))
+
 
 def renderALL(buttonText):
-    buttonText[0][0] = f2.render('Price: 100 Leaves', 1, WHITE)
-    buttonText[0][1] = f2.render('1 Leaf / Second', 1, WHITE)
-    buttonText[1][0] = f2.render('Price: 200 Leaves', 1, WHITE)
-    buttonText[1][1] = f2.render('3 Leaves / Second', 1, WHITE)
-    buttonText[2][0] = f2.render('Price: 500 Leaves', 1, WHITE)
-    buttonText[2][1] = f2.render('9 Leaves / Second', 1, WHITE)
+    buttonText[0][0] = fontButton.render('Price: 100 Leaves', 1, WHITE)
+    buttonText[0][1] = fontButton.render('1 Leaf / Second', 1, WHITE)
+    buttonText[1][0] = fontButton.render('Price: 200 Leaves', 1, WHITE)
+    buttonText[1][1] = fontButton.render('3 Leaves / Second', 1, WHITE)
+    buttonText[2][0] = fontButton.render('Price: 500 Leaves', 1, WHITE)
+    buttonText[2][1] = fontButton.render('9 Leaves / Second', 1, WHITE)
 
 # Подготовка к циклу игры
 running = True
 all_sprites = pygame.sprite.Group()
-button1 = Button((buttonWidth / 2 + 14, 400))
-button2 = Button((3 * buttonWidth / 2 + 14 + whiteSpace, 400))
-button3 = Button((5 * buttonWidth / 2 + 14 + 2 * whiteSpace, 400))
+button1 = Button((buttonWidth / 2 + buttonSpace, buttonY))
+button2 = Button((3 * buttonWidth / 2 + buttonSpace + whiteSpace, buttonY))
+button3 = Button((5 * buttonWidth / 2 + buttonSpace + 2 * whiteSpace, buttonY))
 sprites = [button1, button2, button3]
 all_sprites.add(sprites)
 buttonText = [[0, 0], [0, 0], [0, 0]]
@@ -91,14 +104,14 @@ while running:
 
     # Поддерживаем работу автокликеров
     currentFPS += 1
-    if currentFPS % 60 == 0:
-        score += worker[0] * 1
-        score += worker[1] * 3
-        score += worker[2] * 9
+    if currentFPS % FPS == 0:
+        score += worker[0] * workerProd[0]
+        score += worker[1] * workerProd[1]
+        score += worker[2] * workerProd[2]
 
     # Ввод процесса (события)
     for event in pygame.event.get():
-        if score >= 100000 or event.type == pygame.QUIT:
+        if score >= winScore or event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
@@ -109,7 +122,7 @@ while running:
             score = checkTouch(score, sprites, Click, worker)
 
     # Рендеринг
-    text1 = f1.render('Your leaves : ' + str(score), 1, WHITE)
+    text1 = fontScore.render('Your leaves : ' + str(score), 1, WHITE)
     renderALL(buttonText)
     blitAll(background, text1, screen, buttonText, button1, button2, button3)
     # После отрисовки всего, переворачиваем экран
